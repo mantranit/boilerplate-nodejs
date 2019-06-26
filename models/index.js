864fae99285102834e425b053414bff0a6868700
Jimmy Tran <jimmy.tran@watasolutions.com>
2019-06-26 13:45:57 +0700

    init template model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 module.exports = {
-    User: require('./user.model')
+  User: require('./user.model'),
 };
--- models/user.model.js
+++ models/user.model.js
@@ -2,14 +2,22 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-  email                 : {type: String, required: true, unique: true},
-  firstName             : {type: String, required: true},
-  lastName              : {type: String, required: true},
-  hash                  : {type: String, required: true},
-  role                  : {type: String, enum: ['ADMIN', 'USER'], default: 'USER'},
-  status                : {type: String, enum: ['ACTIVE', 'DEACTIVATED', 'PENDING', 'LOCKED'], default: 'PENDING'},
-  accessToken           : {type: String, default: ''},
-  createdDate           : {type: Date, default: Date.now}
+
+  email: {type: String, required: true, unique: true},
+
+  firstName: {type: String, required: true},
+
+  lastName: {type: String, required: true},
+
+  hash: {type: String, required: true},
+
+  role: {type: String, enum: ['ADMIN', 'USER'], default: 'USER'},
+
+  status: {type: String, enum: ['ACTIVE', 'DEACTIVATED', 'PENDING', 'LOCKED'], default: 'PENDING'},
+
+  accessToken: {type: String, default: ''},
+
+  createdDate: {type: Date, default: Date.now}
 });
 
 schema.set('toJSON', {virtuals: true});
